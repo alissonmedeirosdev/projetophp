0be1e67c8050d62beffe4086038013eccc1115e7
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-03-23 12:14:51 -0300

    styled the main banner

--- index.php
+++ index.php
@@ -3,19 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/f8ec8fcb30.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="static/styles/style.css">
     <title>Projeto PHP</title>
 </head>
 <body>
-
     <!-- header -->
     <header>
         <div class="container">
-            <div class="logo">
-                <!-- TODO: adicionar Logo -->
+            <div class="logo left">
+                LOGOMARCA
             </div>
     
             <!-- navegação desktop -->
-            <nav class="desktop">
+            <nav class="desktop right">
                 <ul>
                     <li>Home</li>
                     <li>Sobre</li>
@@ -25,7 +29,7 @@
             </nav>
     
             <!-- navegação mobile -->
-            <nav class="mobile">
+            <nav class="mobile right">
                 <ul>
                     <li>Home</li>
                     <li>Sobre</li>
@@ -33,6 +37,8 @@
                     <li>Contatos</li>
                 </ul>
             </nav>
+
+            <div class="clear"></div>
         </div>
     </header>
 
@@ -41,7 +47,7 @@
         <div class="container">
             <form action="">
                 <h2>Qual o seu melhor email?</h2>
-                <input type="email" name="email" required>
+                <input type="email" name="email" placeholder="exemple@gmail.com" required >
                 <input type="submit" name="acao" value="cadastrar">
             </form>
         </div>
@@ -50,14 +56,15 @@
     <!-- description -->
     <section class="descricao-autor">
         <div class="container">
-            <div class="w50">
+            <div class="w50 left">
                 <h2 class="title">Sobre</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est adipisci debitis qui nulla odit rem similique, velit, voluptas animi, molestias rerum perferendis quam aliquid quaerat libero dolores corrupti molestiae unde!</p>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, odio velit? Sint magni autem, praesentium est saepe amet, doloremque at quisquam eius aliquid quasi quas odio ipsam earum officia totam.</p>
             </div>
-            <div class="w50">
-                 <img src="img/avatar-image.png" alt="">
+            <div class="w50 left">
+                 <img src="static/img/avatar-image.png" alt="">
             </div>
+            <div class="clear"></div>
         </div>
     </section>
 
@@ -65,36 +72,37 @@
     <section class="especialidades">
         <div class="container">
             <h2 class="title">Especialidades</h2>
-             <div class="box-especialidade">
-                <img src="" alt="">
+             <div class="box-especialidade w33 left">
+                <i class="fa-brands fa-html5"></i>
                 <h3 class="title">HTML</h3>
                 <p class="box-especialidade-description">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda veritatis odit quia. Qui architecto iusto consectetur optio, facere earum vitae assumenda dolorem. Unde, eaque neque illum consequatur quos libero reprehenderit!
                 </p>
              </div>
 
-             <div class="box-especialidade">
-                <img src="" alt="">
+             <div class="box-especialidade w33 left">
+                <i class="fa-brands fa-css3"></i>
                 <h3 class="title">CSS</h3>
                 <p class="box-especialidade-description">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda veritatis odit quia. Qui architecto iusto consectetur optio, facere earum vitae assumenda dolorem. Unde, eaque neque illum consequatur quos libero reprehenderit!
                 </p>
              </div>
 
-             <div class="box-especialidade">
-                <img src="" alt="">
+             <div class="box-especialidade w33 left">
+                <i class="fa-brands fa-js"></i>
                 <h3 class="title">JAVASCRIPT</h3>
                 <p class="box-especialidade-description">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda veritatis odit quia. Qui architecto iusto consectetur optio, facere earum vitae assumenda dolorem. Unde, eaque neque illum consequatur quos libero reprehenderit!
                 </p>
              </div>
+            <div class="clear"></div>
         </div>
     </section>
 
     <!-- extras -->
      <section class="extras">
         <div class="container">
-            <div class="w50">
+            <div class="w50 left">
                 <h2 class="title">Depoimentos</h2>
                 <div class="depoimentos-single">
                     <p class="depoimentos-descicao">
@@ -115,7 +123,7 @@
                 </div>
             </div>
 
-            <div class="w50">
+            <div class="w50 left">
                 <h2 class="title">Serviços</h2>
                 <div class="servicos">
                     <ul>
@@ -125,6 +133,7 @@
                     </ul>
                 </div>
             </div>
+            <div class="clear"></div>
         </div>
      </section>
 
